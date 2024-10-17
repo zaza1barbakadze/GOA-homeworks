5a8c0f8c9f2cf5d724ae3e251f7f624eab959b5d
zaza1barbakadze <zazazdall5@gmail.com>
2024-10-17 20:22:20 +0400

    commit

---
+++ level 43/homework/index.js
@@ -0,0 +1,27 @@
+// 1
+
+const paragraph1 = document.getElementById("p1")
+let word = "ZAZA"
+paragraph1.textContent = word
+
+// 2
+
+const paragraph2 = document.getElementById("p2").style.color = "red"
+
+// 3
+
+const paragraph3 = document.getElementById("p3").style.color = "blue"
+const paragraph3_1 = document.getElementById("p3").style.backgroundColor = "violet"
+
+// 4
+
+const paragraph4_1 = document.getElementById("p4")
+let word2 = "ZaZa"
+paragraph4_1.textContent = word2
+const paragraph4_2 = document.getElementById("p4").style.fontSize = "40px"
+const paragraph4_3 = document.getElementById("p4").style.fontFamily = "cursive"
+
+// 5
+
+const paragraph5 = document.getElementById("p5").style.color = "green"
+const paragraph6 = document.getElementById("p6").style.color = "aqua"
